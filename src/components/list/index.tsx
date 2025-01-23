e1b12d875dbfc1c0090a58533281c415e5d3c373
Talha Khalid <hafizmtalhakhalid@gmail.com>
2025-01-23 21:25:48 +0500

    update

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -11,6 +11,7 @@ import {
   IconButton,
   Drawer,
   useMediaQuery,
+  CircularProgress,
 } from "@mui/material";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import CloseIcon from "@mui/icons-material/Close";
@@ -138,7 +139,10 @@ const UserPosts = () => {
             </Button>
           </Box>
 
-          {/* <Grid container spacing={2} sx={{ padding: 2 }}>
+          {Posts.loading === "fulfilled" ? (
+            <>
+              {" "}
+              {/* <Grid container spacing={2} sx={{ padding: 2 }}>
             {currentPosts?.length > 0 ? (
               currentPosts?.map((item: any, index) => (
                 <Grid item key={index} xs={12} md={4}>
@@ -322,62 +326,64 @@ const UserPosts = () => {
               </Typography>
             )}
           </Grid> */}
-          <Grid container spacing={2} sx={{ padding: 2 }}>
-            {currentPosts?.length > 0 ? (
-              currentPosts?.map((item: any, index) => (
-                <Grid item key={index} xs={12} md={4}>
-                  <Card>
-                    <CardContent>
-                      <Box
-                        sx={{
-                          width: "100%",
-                          height: "200px",
-                          borderRadius: "10px",
-                          overflow: "hidden",
-                        }}
-                      >
-                        <Box
-                          sx={{
-                            width: "100%",
-                            height: "100%",
-                            backgroundImage: `
+              <Grid container spacing={2} sx={{ padding: 2 }}>
+                {currentPosts?.length > 0 ? (
+                  currentPosts?.map((item: any, index) => (
+                    <Grid item key={index} xs={12} md={4}>
+                      <Card>
+                        <CardContent>
+                          <Box
+                            sx={{
+                              width: "100%",
+                              height: "200px",
+                              borderRadius: "10px",
+                              overflow: "hidden",
+                            }}
+                          >
+                            <Box
+                              sx={{
+                                width: "100%",
+                                height: "100%",
+                                backgroundImage: `
     linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
     url(${item?.posted_by?.picture})
   `,
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                          }}
-                        />
-                        {/* Buttons */}
-
-                        <Box
-                          sx={{
-                            display: "flex",
-                            justifyContent: "flex-end",
-                            alignItems: "center",
-                            gap: "10px",
-                            position: "absolute",
-                            top: "6%",
-                            right: "6%",
-                          }}
-                        >
-                          {profile?.profile?.profile?.id ===
-                            item?.posted_by?.id && (
-                            <Button
-                              variant="outlined"
-                              size="small"
-                              sx={{
-                                backgroundColor: "rgba(255, 255, 255, 0.2)",
-                                minWidth: "40px",
-                                color: "#fff",
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
                               }}
-                              onClick={() => navigate(`/edit/post/${item.id}`)}
-                            >
-                              <Iconify icon="cuida:edit-outline" />
-                            </Button>
-                          )}
+                            />
+                            {/* Buttons */}
 
-                          {/* <Button
+                            <Box
+                              sx={{
+                                display: "flex",
+                                justifyContent: "flex-end",
+                                alignItems: "center",
+                                gap: "10px",
+                                position: "absolute",
+                                top: "6%",
+                                right: "6%",
+                              }}
+                            >
+                              {profile?.profile?.profile?.id ===
+                                item?.posted_by?.id && (
+                                <Button
+                                  variant="outlined"
+                                  size="small"
+                                  sx={{
+                                    backgroundColor: "rgba(255, 255, 255, 0.2)",
+                                    minWidth: "40px",
+                                    color: "#fff",
+                                  }}
+                                  onClick={() =>
+                                    navigate(`/edit/post/${item.id}`)
+                                  }
+                                >
+                                  <Iconify icon="cuida:edit-outline" />
+                                </Button>
+                              )}
+
+                              {/* <Button
                             variant="outlined"
                             size="small"
                             sx={{
@@ -388,21 +394,21 @@ const UserPosts = () => {
                           >
                             <Iconify icon="lucide:move-right" />
                           </Button> */}
-                        </Box>
-                      </Box>
+                            </Box>
+                          </Box>
 
-                      <Box>
-                        <Box
-                          sx={{
-                            textAlign: "left",
-                            marginTop: 2,
-                            display: "flex",
-                            gap: "10px",
-                            justifyContent: "start",
-                            paddingLeft: "12px",
-                          }}
-                        >
-                          {/* <Avatar
+                          <Box>
+                            <Box
+                              sx={{
+                                textAlign: "left",
+                                marginTop: 2,
+                                display: "flex",
+                                gap: "10px",
+                                justifyContent: "start",
+                                paddingLeft: "12px",
+                              }}
+                            >
+                              {/* <Avatar
                             alt={item?.posted_by?.user?.name}
                             src={item?.posted_by?.picture}
                             sx={{
@@ -410,79 +416,81 @@ const UserPosts = () => {
                               height: 50,
                             }}
                           /> */}
-                          <Box
-                            sx={{
-                              display: "flex",
-                              alignItems: "flex-start",
-                              flexDirection: "column",
-                            }}
-                          >
-                            <Typography variant="h6">
-                              {item?.posted_by?.user?.name}
-                            </Typography>
-                            <Typography
-                              variant="body2"
-                              sx={{ fontSize: "12px" }}
-                            >
-                              {new Date(item?.posted_on).toLocaleDateString()}
-                            </Typography>
-                            <Typography
-                              sx={{ fontSize: "13px", fontWeight: "bold" }}
-                            >
-                              {item.travel_to_city}
-                              {", "}
-                              {item.travel_to_country}
-                            </Typography>
-                            <Typography
-                              variant="body2"
-                              noWrap
-                              style={{
-                                overflow: "hidden",
-                                textOverflow: "ellipsis",
-                                whiteSpace: "nowrap",
-                                width: "260px",
+                              <Box
+                                sx={{
+                                  display: "flex",
+                                  alignItems: "flex-start",
+                                  flexDirection: "column",
+                                }}
+                              >
+                                <Typography variant="h6">
+                                  {item?.posted_by?.user?.name}
+                                </Typography>
+                                <Typography
+                                  variant="body2"
+                                  sx={{ fontSize: "12px" }}
+                                >
+                                  {new Date(
+                                    item?.posted_on
+                                  ).toLocaleDateString()}
+                                </Typography>
+                                <Typography
+                                  sx={{ fontSize: "13px", fontWeight: "bold" }}
+                                >
+                                  {item.travel_to_city}
+                                  {", "}
+                                  {item.travel_to_country}
+                                </Typography>
+                                <Typography
+                                  variant="body2"
+                                  noWrap
+                                  style={{
+                                    overflow: "hidden",
+                                    textOverflow: "ellipsis",
+                                    whiteSpace: "nowrap",
+                                    width: "260px",
+                                  }}
+                                >
+                                  {item?.text}
+                                </Typography>
+                              </Box>
+                            </Box>
+                            <Box
+                              sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                width: "100%",
                               }}
+                              mt={2}
                             >
-                              {item?.text}
-                            </Typography>
+                              <Button
+                                onClick={() => navigate(`/details/${item?.id}`)}
+                                variant="contained"
+                                fullWidth
+                              >
+                                See Details
+                              </Button>
+                            </Box>
                           </Box>
-                        </Box>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            width: "100%",
-                          }}
-                          mt={2}
-                        >
-                          <Button
-                            onClick={() => navigate(`/details/${item?.id}`)}
-                            variant="contained"
-                            fullWidth
-                          >
-                            See Details
-                          </Button>
-                        </Box>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))
-            ) : (
-              <Typography
-                variant="h6"
-                sx={{
-                  margin: "auto",
-                  marginTop: 4,
-                  textAlign: "center",
-                  color: "#666",
-                }}
-              >
-                No posts found matching the filters.
-              </Typography>
-            )}
-          </Grid>
-          {/* <Grid container spacing={2} sx={{ padding: 2 }}>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))
+                ) : (
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      margin: "auto",
+                      marginTop: 4,
+                      textAlign: "center",
+                      color: "#666",
+                    }}
+                  >
+                    No posts found matching the filters.
+                  </Typography>
+                )}
+              </Grid>
+              {/* <Grid container spacing={2} sx={{ padding: 2 }}>
             {currentPosts?.length > 0 ? (
               currentPosts?.map((item: any, index) => (
                 <Grid item key={index} xs={12} md={4}>
@@ -632,20 +640,33 @@ const UserPosts = () => {
               </Typography>
             )}
           </Grid> */}
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              padding: 3,
-            }}
-          >
-            <Pagination
-              count={Math.ceil(filteredPosts.length / postsPerPage)}
-              page={page}
-              onChange={handlePageChange}
-              color="primary"
-            />
-          </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  padding: 3,
+                }}
+              >
+                <Pagination
+                  count={Math.ceil(filteredPosts.length / postsPerPage)}
+                  page={page}
+                  onChange={handlePageChange}
+                  color="primary"
+                />
+              </Box>
+            </>
+          ) : (
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          )}
         </Box>
       </Box>
     </>
